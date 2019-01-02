9424a1b0ea3f3d74fa268ef2bb86c06c21ecdb2e
PiotrBinkowski <33829581+PiotrBinkowski@users.noreply.github.com>
2019-01-03 00:05:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+main.py
+
+This is a python file wich prints some attributes of the song "Smells Like Teen Spirit"
+'''
+
+#string variables
+Band = "Nirvana"
+Genre = "Grunge"
+Album = "Nevermind"
+Title = "Smells Like Teen Spirit"
+GuitarVocals, Bass,  Drums = "Kurt Cobain", "Krist Novoselic", "Dave Grohl"
+Artists = 'guitar, vocals: ' + GuitarVocals + '\nbass: ' + Bass + '\ndrums: ' + Drums
+
+#integer variables
+DurationInSeconds = 301
+YearReleased = 1991
+
+#float variables
+DurationInMinutes = 5.01
+
+print(Band)
+print(Genre)
+print(Album)
+print(Title)
+print(Artists)
+print(DurationInSeconds)
+print(DurationInMinutes)
+print(YearReleased)
